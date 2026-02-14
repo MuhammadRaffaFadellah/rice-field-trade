4dba87839190b9b8613925f4084f18b124a5e5bd
Muhammad Raffa Fadellah <muhammadraffafadellah@smkinformatika-crbn.sch.id>
2026-02-15 00:39:13 +0700

    regist page progress #1

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="id">
 
 <head>
@@ -65,6 +65,30 @@
         </form>
     </div>
 
+</body>
+
+</html> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Registrasi - Rice field trade</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="h-screen flex items-center justify-center bg-[#f5f8fc]">
+        <div class="w-200 h-150 bg-amber-300">
+            <div class="regist-header flex justify-center items-center">
+                <h1 class="">Daftar</h1>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
